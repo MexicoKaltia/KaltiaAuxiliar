a30c562a244b532643d363ea58f9915cf2b96029
Hugo G RIVAS <hugo.rivas@vun.mx>
2018-05-23 17:44:43 -0500

    230528

--- kaltiaDB/kaltiadb_tc_action.sql
+++ kaltiaDB/kaltiadb_tc_action.sql
@@ -44,7 +44,7 @@ CREATE TABLE `tc_action` (
 
 LOCK TABLES `tc_action` WRITE;
 /*!40000 ALTER TABLE `tc_action` DISABLE KEYS */;
-INSERT INTO `tc_action` VALUES ('abele','abele','abele',1,1,1,1,1,1,NULL,NULL),('actionA','idEmpresaA','bronea',1,1,1,0,0,1,NULL,NULL),('aerogem','aerogem','aerogem',1,1,1,NULL,NULL,1,NULL,NULL),('algenius','algenius','algenius',1,1,1,NULL,NULL,1,NULL,NULL),('appsea','appsea','appsea',1,1,1,0,0,1,NULL,NULL),('arialogic','arialogic','arialogic',1,1,1,0,NULL,1,NULL,NULL),('arrival','arrival','arrival',1,1,1,NULL,NULL,1,NULL,NULL),('artland','artland','artland',1,1,1,NULL,NULL,1,NULL,NULL),('bronea','bronea','bronea',1,1,1,NULL,NULL,1,NULL,NULL),('carinary','carinary','carinary',1,1,1,0,0,1,NULL,NULL),('hambur','hambur','skaxis',1,1,1,0,0,1,NULL,NULL),('kaltia','Kaltia','viral',1,1,1,NULL,NULL,1,NULL,NULL),('menuA','panini','bronea',1,NULL,NULL,NULL,NULL,1,NULL,NULL),('PuroCuero','PuroCuero','skaxis',1,1,1,0,0,1,NULL,NULL),('skaxis','skaxis','skaxis',1,1,1,NULL,NULL,1,NULL,NULL),('viral','viral','viral',1,1,1,NULL,NULL,1,NULL,NULL),('vortex','Vortex','vortex',1,1,1,0,0,1,'',NULL);
+INSERT INTO `tc_action` VALUES ('abele','abele','abele',1,1,1,1,1,1,NULL,NULL),('actionA','idEmpresaA','bronea',1,1,1,0,0,1,NULL,NULL),('aerogem','aerogem','aerogem',1,1,1,NULL,NULL,1,NULL,NULL),('algenius','algenius','algenius',1,1,1,NULL,NULL,1,NULL,NULL),('appsea','appsea','appsea',1,1,1,0,0,1,NULL,NULL),('arialogic','arialogic','arialogic',1,1,1,0,NULL,1,NULL,NULL),('arrival','arrival','arrival',1,1,1,NULL,NULL,1,NULL,NULL),('artland','artland','artland',1,1,1,NULL,NULL,1,NULL,NULL),('bronea','bronea','bronea',1,1,1,NULL,NULL,1,NULL,NULL),('carinary','carinary','carinary',1,1,1,0,0,1,NULL,NULL),('hambur','hambur','skaxis',1,1,1,0,0,1,NULL,NULL),('kaltia','Kaltia','viral',1,1,1,NULL,NULL,1,NULL,NULL),('mazda','Mazda','bronea',1,1,1,NULL,NULL,1,NULL,NULL),('menuA','panini','bronea',1,NULL,NULL,NULL,NULL,1,NULL,NULL),('PuroCuero','PuroCuero','skaxis',1,1,1,0,0,1,NULL,NULL),('skaxis','skaxis','skaxis',1,1,1,NULL,NULL,1,NULL,NULL),('viral','viral','viral',1,1,1,NULL,NULL,1,NULL,NULL),('vortex','Vortex','vortex',1,1,1,0,0,1,'',NULL);
 /*!40000 ALTER TABLE `tc_action` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-17 19:02:57
+-- Dump completed on 2018-05-23 17:13:18
