367f6a3c3a01310bb3b647789bde2da95bd9730d
Hugo G RIVAS <hugo.rivas@vun.mx>
2018-07-26 18:44:31 -0500

    260718

---
+++ test/modal/js/complemento.js
@@ -0,0 +1,29 @@
+$(function() {
+$("#upload-file-input").on("change", uploadFile);
+});
+
+/**
+ * Upload the file sending it via Ajax at the Spring Boot server.
+ */
+function uploadFile() {
+  $.ajax({
+    url: "http://localhost:8010/fileUpload",
+    type: "POST",
+    data: new FormData($("#upload-file-form")[0]),
+    enctype: 'multipart/form-data',
+    processData: false,
+    contentType: false,
+    cache: false,
+    success: function () {
+      // Handle upload success
+      // ...
+      alert("exito")
+    },
+    error: function () {
+      // Handle upload error
+      // ...
+      alert("negative")
+    }
+  });
+} // function uploadFile
+
