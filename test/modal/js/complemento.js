1f89078b10a19b596268f1628b5f4afd32e8f6c8
Hugo G RIVAS <hugo.rivas@vun.mx>
2018-08-15 17:43:46 -0500

    150818

--- test/modal/js/complemento.js
+++ test/modal/js/complemento.js
@@ -35,3 +35,30 @@ function uploadFile() {
       $('.headerSeccion3').attr("data-target","#modalEdicion");
   });
 
+$(document).ready(function() {
+
+  $('#openBtn').click(function() {
+    $('#myModal').modal({
+      show: true
+    })
+  });
+
+  $(document).on({
+    'show.bs.modal': function() {
+      var zIndex = 1040 + (10 * $('.modal:visible').length);
+      $(this).css('z-index', zIndex);
+      setTimeout(function() {
+        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+      }, 0);
+    },
+    'hidden.bs.modal': function() {
+      if ($('.modal:visible').length > 0) {
+        // restore the modal-open class to the body element, so that scrolling works
+        // properly after de-stacking a modal.
+        setTimeout(function() {
+          $(document.body).addClass('modal-open');
+        }, 0);
+      }
+    }
+  }, '.modal');
+});
