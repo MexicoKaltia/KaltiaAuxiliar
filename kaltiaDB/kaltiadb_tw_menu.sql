89b2bec4dcd5c3a501d41852b938d9c8323eb643
hrivascortes <hrivas.cortes@gmail.com>
2018-10-14 13:29:29 -0500

    141018

---
+++ kaltiaDB/kaltiadb_tw_menu.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: kaltiadb
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tw_menu`
+--
+
+DROP TABLE IF EXISTS `tw_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tw_menu` (
+  `idMenu` char(15) NOT NULL,
+  `menuA` json DEFAULT NULL,
+  `menuSubA` json DEFAULT NULL,
+  `menuSubSubA` json DEFAULT NULL,
+  `menuJson` json DEFAULT NULL,
+  PRIMARY KEY (`idMenu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tw_menu`
+--
+
+LOCK TABLES `tw_menu` WRITE;
+/*!40000 ALTER TABLE `tw_menu` DISABLE KEYS */;
+INSERT INTO `tw_menu` VALUES ('idMenuActionA','{\"menuA\": [\"Home\", \"Pages\", \"Dropdown\", \"Link Text1\", \"Link Text0\", \"Link Text2\"]}','{\"Pages\": [\"pages1\", \"pages3\"], \"Dropdown\": [\"dor\", \"dor2\", \"dor3\"]}','{\"dor\": [\"dorA\", \"dorB\", \"dorC\"], \"pages3\": [\"pages3A\", \"pages6\", \"pages8\"]}','{\"menuA\": [\"Home\", \"Pages\", \"Dropdown\", \"Link Text1\", \"Link Text0\", \"Link Text2\"], \"menuSubA\": [{\"Pages\": [\"pages1\", \"pages3\"], \"Dropdown\": [\"dor\", \"dor2\", \"dor3\"]}], \"menuSubSubA\": [{\"dor\": [\"dorA\", \"dorB\", \"dorC\"], \"pages3\": [\"pages3A\", \"pages6\", \"pages8\"]}]}');
+/*!40000 ALTER TABLE `tw_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-14 13:27:35
