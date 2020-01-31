1881be89aaca8fdc0729fde6f5eedc066a7d88f2
hrivascortes <hrivas.cortes@gmail.com>
2020-01-30 22:28:06 -0600

    300120

--- kaltiaProyectos/Uniprotec/js/complemento.js
+++ kaltiaProyectos/Uniprotec/js/complemento.js
@@ -43,7 +43,33 @@ $(document).ready(function(){
     function limpiaAlerta(){
 		$( "div" ).remove( ".alerta_in" );
 	}
-		
+
+
+/*
+*	REST
+*/
+var username = $("input#username").val();
+var password = $("input#password").val();  
+
+function make_base_auth(user, password) {
+  var tok = user + ':' + password;
+  var hash = btoa(tok);
+  return "Basic " + hash;
+}
+$.ajax
+  ({
+    type: "GET",
+    url: "index1.php",
+    dataType: 'json',
+    async: false,
+    data: '{}',
+    beforeSend: function (xhr){ 
+        xhr.setRequestHeader('Authorization', make_base_auth(username, password)); 
+    },
+    success: function (){
+        alert('Thanks for your comment!'); 
+    }
+});		
 }); // Fin documento
 
 
